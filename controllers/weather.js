5310dcf68a54aa946277cdb12099601fc4ba8f5e
RampantOwl <trygve@grimnes.org>
2014-12-23 11:30:47 +0100

    Deleting db entires when uploading new file

--- controllers/weather.js
+++ controllers/weather.js
@@ -47,6 +47,49 @@ function buildQuery( field, type , month , day, showHours , startDate , endDate)
 
 	return query
 }
+
+function readWeatherFile( filename , callback){
+	var weatherInstances = new Array()
+	
+	fs.readFile( './uploads/'+filename , "ucs2" , function( err , data ){
+		var lines = data.split("\r")
+		for( var i = 1; data.length > i; i++){
+			if( typeof(lines[i] ) == "undefined" )
+				continue				
+			var tmp = lines[i].replace("\n","").split("\t")
+			
+			if( typeof( tmp[1] ) == "undefined" )
+				continue
+			var dateRaw = tmp[1].split(".")
+			var date = new Date( dateRaw[1] + "." + dateRaw[0] + "." + dateRaw[2] )
+			var structure = {
+				no : tmp[0],
+				time : date,
+				interval : tmp[2],
+				tempInside : tmp[3],
+				airMoistInside : tmp[4],
+				tempOutside : tmp[5],
+				airMoistOutside : tmp[6],
+				relHPA : tmp[7],
+				absHPA : tmp[8],
+				windSpeed : tmp[9],
+				windBy : tmp[10],
+				windDir : tmp[11],
+				dewPoint : tmp[12],
+				windTemp : tmp[13],
+				rainprhour : tmp[14],
+				rainlasttwentyfour : tmp[15],
+				rainlastweek : tmp[16],
+				rainlastmonth : tmp[17],
+				raintotal : tmp[18]
+			}
+
+			var weatherlog = new WeatherLog( structure )
+			weatherInstances.push( weatherlog )
+		}
+		return callback( null, weatherInstances )
+	})
+}
 module.exports = {
 	
 	getlog : function( req , res ){
@@ -80,54 +123,21 @@ module.exports = {
 	},
 	importfile : function( req , res ){
 		var filename = req.files.thumbnail.name
-		var weatherInstances = new Array()
+		
 		var password = req.body.password
 
 		if( password != "grimnes" )
 			return res.send( "Feil passord" )
 		
-		fs.readFile( './uploads/'+filename , "ucs2" , function( err , data ){
-			var lines = data.split("\r")
-			for( var i = 1; data.length > i; i++){
-				if( typeof(lines[i] ) == "undefined" )
-					continue				
-				var tmp = lines[i].replace("\n","").split("\t")
-				
-				if( typeof( tmp[1] ) == "undefined" )
-					continue
-				var dateRaw = tmp[1].split(".")
-				var date = new Date( dateRaw[1] + "." + dateRaw[0] + "." + dateRaw[2] )
-					var structure = {
-						no : tmp[0],
-						time : date,
-						interval : tmp[2],
-						tempInside : tmp[3],
-						airMoistInside : tmp[4],
-						tempOutside : tmp[5],
-						airMoistOutside : tmp[6],
-						relHPA : tmp[7],
-						absHPA : tmp[8],
-						windSpeed : tmp[9],
-						windBy : tmp[10],
-						windDir : tmp[11],
-						dewPoint : tmp[12],
-						windTemp : tmp[13],
-						rainprhour : tmp[14],
-						rainlasttwentyfour : tmp[15],
-						rainlastweek : tmp[16],
-						rainlastmonth : tmp[17],
-						raintotal : tmp[18]
-					}
-
-				var weatherlog = new WeatherLog( structure )
-				weatherInstances.push( weatherlog )
-			}
+		readWeatherFile( filename , function( err, weatherInstances ){
 			console.log( "Got %s" , weatherInstances.length )
-			async.forEach( weatherInstances, function( w , callback){
-				w.save( callback )
-			}, function( err , done ){
-				res.redirect( "/" )
-				//res.render( 'upload_done', { line_count : weatherInstances.length } )
+			WeatherLog.remove({},function(){
+				async.forEach( weatherInstances, function( w , callback){
+					w.save( callback )
+				}, function( err , done ){
+					res.redirect( "/" )
+					//res.render( 'upload_done', { line_count : weatherInstances.length } )
+				})
 			})
 		})
 	}
